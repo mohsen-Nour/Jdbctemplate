f0e52c122c08babb31c86a46305e7d6ebd8133f0
mohsen-Nour <mohsen.noor@gmail.com>
2017-10-27 23:27:19 +0330

    switch to postgres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-insert into account (owner_name,balance,access_time,locked) values ('john doe',10.0,'2014-01-01',false);
+insert into bank.account (id,owner_name,balance,access_time,locked) values (1,'john doe',10.0,'2014-01-01',false);
